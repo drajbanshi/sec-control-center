6af43c90dc249b66fddb85f0f8fe020a539f6046
dev.rajbanshi <dev.rajbanshi@gmail.com>
2015-02-02 23:42:09 -0500

    fixes

--- scc-web/grails-app/services/com/freddiemac/SearchService.groovy
+++ scc-web/grails-app/services/com/freddiemac/SearchService.groovy
@@ -8,8 +8,7 @@ import groovy.io.FileType
 class SearchService {
 
     def searchPool(String cus) {
-
-      /*  SOAPClient client = new SOAPClient('http://172.20.17.7:9999/freddiemac/services/searchload.asmx')
+        SOAPClient client = new SOAPClient('http://localhost:9999/freddiemac/services/searchload.asmx')
         def response = client.send(SOAPAction:'FindLoan') {
             body {
                 FindLoan('xmlns':'http://www.freddiemac.com/search') {
@@ -17,38 +16,9 @@ class SearchService {
                 }
             }
         }
-		  //def responseXML = new File('C:/Users/c36429/git/sec-control-center/stub-server/__files/cusp_output.xml').getText('CUSP_1')
-		 // return responseXML
-		//println responseXML
-       
-*/		
-		def inputfile;
-	def list = []
-	def isExist
-		def dir = new File('C:/Users/c36429/git/sec-control-center/stub-server/__files/')
-		dir.eachFileRecurse (FileType.FILES) { file ->
-		  list << file
-	} 
-		  list.each {
-			   def filename= it.getName()
-			 	   
-			   inputfile= cus+".xml"
-			   if(filename == inputfile)
-			   {
-					 isExist = "Available"
-				 
-			   }
-			  }
-		  
-		  if(isExist!="Available")
-		  
-		  {	   
-			  isExist = "Not available"
-			  return isExist
-		 }
-		  def filePath = "C:/Users/c36429/git/sec-control-center/stub-server/__files/"+inputfile
-		  println "filepath : "+filePath
-		  def responseXML = new File(filePath).getText('UTF-8')
-		  return responseXML
-    }
+		
+		println response.text
+		
+		return response
+	}
 }
--- stub-server/src/main/java/com/freddiemac/StubHttpProxy.java
+++ stub-server/src/main/java/com/freddiemac/StubHttpProxy.java
@@ -1,6 +1,8 @@
 package com.freddiemac;
 
 import java.io.File;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.common.SingleRootFileSource;
@@ -12,7 +14,12 @@ public class StubHttpProxy {
 	public static void main(String[] args) {
 		File f = new File(System.getProperty("user.dir"));
 		System.out.println(f.getAbsolutePath());
-		WireMockServer server = new WireMockServer(new WireMockConfiguration().port(9999).fileSource(new SingleRootFileSource(f)));
+		WireMockServer server = new WireMockServer(new WireMockConfiguration().port(9999).fileSource(new SingleRootFileSource(f)).proxyVia("bproxy", 8080));
+		Authenticator.setDefault(new Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("c38051","P@ssw0rd".toCharArray());
+			}
+		});
 		server.start();
 	}
 
