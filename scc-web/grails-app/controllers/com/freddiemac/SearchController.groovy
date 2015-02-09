bfcce48ad9ae720b0b605a32a9616c426943a746
dev.rajbanshi <dev.rajbanshi@gmail.com>
2015-02-09 12:00:48 -0500

    refactoring

--- scc-web/grails-app/controllers/com/freddiemac/SearchController.groovy
+++ scc-web/grails-app/controllers/com/freddiemac/SearchController.groovy
@@ -7,7 +7,7 @@ import com.freddiemac.model.PropContainer
 
 class SearchController {
 	def searchService
-	
+
 	def dispatchService
 
 	def grailsApplication
@@ -21,7 +21,7 @@ class SearchController {
 			redirect action: 'index', method: "Get"
 		}
 		def m = searchService.searchPool(params.cusip)
-		
+
 		if (!m.success) {
 			flash.error =  "Pool Unavailable for CUSIP ID=${params.cusip}"
 			render view: 'index'
@@ -29,44 +29,43 @@ class SearchController {
 			flash.message = EventProcessLog.findByCusip(params?.cusip)!=null? "CUSIP ID=${params.cusip} is already sent for Dissolve process": ''
 			render view: 'index', model: ['result': generateModel( PropertyRetriever.getProp(grailsApplication.config.com.freddiemac.businessdata.path, m.events)), isDissolve:flash.message!='']
 		}
-		
-		
 	}
-	
-	
+
+
 	private def generateModel(def m) {
 		def keys = grailsApplication.config.com.freddiemac.onedotfive.interf
 		def mm = []
 		keys.each {
-		    mm.add(new PropContainer(key: it, value: PropertyRetriever.getProp(it, m)))
+			mm.add(new PropContainer(key: it, value: PropertyRetriever.getProp(it, m)))
 		}
 		return mm
 	}
-	
+
 	def dissolve() {
 		if (!params.cusip) {
 			flash.message =  "Invalid cusip"
 			redirect action: 'index'
 			return
 		}
-		
+
 		def m = searchService.searchPool(params.cusip)
 		m.events.EventMetaData.EventName = 'DISSOLVE_EVENT'
-		
-	//	print XmlUtil.serialize(m)
-		
+
+
 		EventProcessLog e = new EventProcessLog(cusip: params.cusip, status: Status.INITIALIZED, eventType: EventType.DISSOLVE)
 		e.save()
-		
-		if(dispatchService.dissolveSecurity(params.cusip)) {
-			flash.message = "Dissolve event sent successfully"
-		} else {
-		   flash.message  = "Error sending dissolve event"
-		}
-		
-		
-		
-		render view: 'index', model: ['result': PropertyRetriever.getProp(grailsApplication.config.com.freddiemac.security.node.path, m.events),'isDissolve':true]
 
+		if(m.success) {
+			if(dispatchService.dissolveSecurity(m.events)) {
+				flash.message = "Dissolve event sent successfully"
+			} else {
+				flash.message  = "Error sending dissolve event"
+			}
+		} else {
+			flash.error =  "Pool Unavailable for CUSIP ID=${params.cusip}"
+		}
+
+
+		render view: 'index', model: ['result': generateModel( PropertyRetriever.getProp(grailsApplication.config.com.freddiemac.businessdata.path, m.events)), isDissolve:flash.message!='']
 	}
 }
