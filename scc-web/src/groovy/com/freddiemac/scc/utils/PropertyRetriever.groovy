222c742616e4c6e9dc0510e9f0f939260d4fb26e
dev.rajbanshi <dev.rajbanshi@gmail.com>
2015-02-11 14:13:29 -0500

    gpath fix

--- scc-web/src/groovy/com/freddiemac/scc/utils/PropertyRetriever.groovy
+++ scc-web/src/groovy/com/freddiemac/scc/utils/PropertyRetriever.groovy
@@ -1,18 +1,17 @@
 package com.freddiemac.scc.utils
 
 class PropertyRetriever {
-	
-    static def getProp(String propx, def o) {
+
+	static def getProp(String propx, def o) {
 		if(!o) {
 			return null
 		}
 		def r = o
 		for(def p : propx.split("\\.")) {
-			r = r."${p}"
-		   if(r == null) {
-			   return null
-		   }
-			
+			r = r[p]
+			if(!r) {
+				return null
+			}
 		}
 		return r
 	}
