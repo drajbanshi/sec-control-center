4707520a6c13d8f1cbb4a55577087ac6d7bb9596
Vikram S <>
2015-02-19 16:04:13 -0500

    Updated the validation for special characters

--- scc-web/src/groovy/com/freddiemac/scc/model/PoolSearch.groovy
+++ scc-web/src/groovy/com/freddiemac/scc/model/PoolSearch.groovy
@@ -6,15 +6,19 @@ class PoolSearch {
 	
 String cusipIdentifier
 String poolNumber
+
 static constraints = {
     cusipIdentifier blank:true, nullable: true
     poolNumber blank:true, nullable: true
+    def expression = '^[A-Za-z0-9\\d]*$' 
     cusipIdentifier validator: { val, obj ->
             if (!val && !obj.poolNumber) return 'PoolSearch.searchCriteria.atleastonerequired'
             if (val && obj.poolNumber) return 'PoolSearch.searchCriteria.bothentered'
             if (val!=null && val.length() > 10) return 'PoolSearch.searchCriteria.cusipIdentifier.size'
             if (obj.poolNumber!=null && obj.poolNumber.length() > 10) return 'PoolSearch.searchCriteria.poolNumber.size'
-            //if (val!=null && val=`) return 'PoolSearch.searchCriteria.cusipIdentifier.size'
+            if(val!=null && !val.matches(expression)) return 'PoolSearch.searchCriteria.cusipIdentifier.specialchar'            
+            if(obj.poolNumber!=null && !obj.poolNumber.matches(expression)) return 'PoolSearch.searchCriteria.poolNumber.specialchar'
+
         }
     }
 }
