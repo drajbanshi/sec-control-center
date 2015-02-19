0703383c9ac89d21e256ecac725ececf1c10e3e6
Vikram S <>
2015-02-19 11:04:38 -0500

    Added the search validations

--- scc-web/src/groovy/com/freddiemac/scc/model/PoolSearch.groovy
+++ scc-web/src/groovy/com/freddiemac/scc/model/PoolSearch.groovy
@@ -10,11 +10,11 @@ static constraints = {
     cusipIdentifier blank:true, nullable: true
     poolNumber blank:true, nullable: true
     cusipIdentifier validator: { val, obj ->
-            if (!val && !obj.poolNumber) return 'PoolSearch.searchCriteria.atleastOneRequired'
-            /*else {
-                if (val)
-            }*/
-        
+            if (!val && !obj.poolNumber) return 'PoolSearch.searchCriteria.atleastonerequired'
+            if (val && obj.poolNumber) return 'PoolSearch.searchCriteria.bothentered'
+            if (val!=null && val.length() > 10) return 'PoolSearch.searchCriteria.cusipIdentifier.size'
+            if (obj.poolNumber!=null && obj.poolNumber.length() > 10) return 'PoolSearch.searchCriteria.poolNumber.size'
+            //if (val!=null && val=`) return 'PoolSearch.searchCriteria.cusipIdentifier.size'
         }
     }
 }
