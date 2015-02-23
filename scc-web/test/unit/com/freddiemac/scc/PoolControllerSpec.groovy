93916b9c307b0a611ac7ae50c1c023f9dc5daff7
dev.rajbanshi <dev.rajbanshi@gmail.com>
2015-02-23 13:53:53 -0500

    better testcase

--- scc-web/test/unit/com/freddiemac/scc/PoolControllerSpec.groovy
+++ scc-web/test/unit/com/freddiemac/scc/PoolControllerSpec.groovy
@@ -67,7 +67,7 @@ class PoolControllerSpec extends Specification {
 	}
 
 	@Unroll
-	def "search validation works with #cusip, #poolid"(String cusip, String poolid) {
+	def "search validation works with cusip=#cusip, poolid=#poolid"(String cusip, String poolid) {
 		given:
 		PoolSearch poolSearch = new PoolSearch(cusipIdentifier: cusip, poolNumber: poolid)
 		poolSearch.validate()
@@ -89,7 +89,7 @@ class PoolControllerSpec extends Specification {
 	}
 
 	@Unroll
-	def "search with valid params and pool available works"(String cusip, String poolid, boolean collapsed) {
+	def "search with valid params and pool available works (cusip=#cusip, poolid=#poolid)"(String cusip, String poolid, boolean collapsed) {
 		given:
 		PoolSearch poolSearch = new PoolSearch(cusipIdentifier: cusip, poolNumber: poolid)
 		poolSearch.validate()
@@ -112,7 +112,7 @@ class PoolControllerSpec extends Specification {
 	}
 
 	@Unroll
-	void "valid cusip set but Pool Unavailable"(String cusip, String poolid, String err) {
+	void "valid cusip set but Pool Unavailable(cusip=#cusip, poolid=#poolid, err = #err)"(String cusip, String poolid, String err) {
 		when:
 		controller.search(new PoolSearch(cusipIdentifier: cusip, poolNumber: poolid))
 
@@ -127,7 +127,7 @@ class PoolControllerSpec extends Specification {
 	}
 	
 	@Unroll
-	void "collapse pool works with validation"(String cusip, String poolid, String err) {
+	void "collapse pool works with validation (cusip=#cusip, poolid=#poolid, err = #err)"(String cusip, String poolid, String err) {
 		given:
 		params.cusip = cusip
 		params.poolid = poolid
