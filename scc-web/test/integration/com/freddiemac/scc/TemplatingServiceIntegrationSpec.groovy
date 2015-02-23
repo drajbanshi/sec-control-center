69c54d2218002e2a4ed5656fda5479db54aa6379
dev.rajbanshi <dev.rajbanshi@gmail.com>
2015-02-23 15:50:05 -0500

    test coverage report

--- scc-web/test/integration/com/freddiemac/scc/TemplatingServiceIntegrationSpec.groovy
+++ scc-web/test/integration/com/freddiemac/scc/TemplatingServiceIntegrationSpec.groovy
@@ -9,11 +9,13 @@ class TemplatingServiceIntegrationSpec extends IntegrationSpec {
     void "test collapse event xml"() {
 		when:
 		String poolId = "abcdef"
-		String xml = xmlTemplatingService.generateCollapseEvent(poolId)
+		String poolType ="CASH"
+		String xml = xmlTemplatingService.generateCollapseEvent(poolId,poolType)
 		print xml
 		then:
 		assert xml != null && !xml.isEmpty()
 		assert xml.contains(poolId)
+		assert xml.contains(poolType)
 		
     }
 }
