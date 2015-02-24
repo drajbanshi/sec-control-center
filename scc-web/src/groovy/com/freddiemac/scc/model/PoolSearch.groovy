202bc45709524f45cacf21733a999df0dcaa0e35
Vikram S <>
2015-02-24 15:01:15 -0500

    fixed the error with command validation for cusip & pool

--- scc-web/src/groovy/com/freddiemac/scc/model/PoolSearch.groovy
+++ scc-web/src/groovy/com/freddiemac/scc/model/PoolSearch.groovy
@@ -11,14 +11,14 @@ static constraints = {
     cusipIdentifier blank:true, nullable: true
     poolNumber blank:true, nullable: true
     def expression = '^[A-Za-z0-9\\d]*$' 
-    cusipIdentifier validator: { val, obj ->
-            if (!val && !obj.poolNumber) return 'PoolSearch.searchCriteria.atleastonerequired'
-            if (val && obj.poolNumber) return 'PoolSearch.searchCriteria.bothentered'
+    cusipIdentifier validator: { val ->
             if (val!=null && val.length() > 10) return 'PoolSearch.searchCriteria.cusipIdentifier.size'
             if(val!=null && !val.matches(expression)) return 'PoolSearch.searchCriteria.cusipIdentifier.specialchar'            
 
         }
-    poolNumber validator: { val  ->
+    poolNumber validator: { val, obj  ->
+            if (!val && !obj.cusipIdentifier) return 'PoolSearch.searchCriteria.atleastonerequired'
+            if (val && obj.cusipIdentifier) return 'PoolSearch.searchCriteria.bothentered'        
             if (val!=null && val.length() > 10) return 'PoolSearch.searchCriteria.poolNumber.size'
             if(val!=null && !val.matches(expression)) return 'PoolSearch.searchCriteria.poolNumber.specialchar'
         }  
