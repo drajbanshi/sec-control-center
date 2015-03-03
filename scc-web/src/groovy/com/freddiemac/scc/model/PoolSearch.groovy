7b3a45bae154df90682a1f6b77fa044665fe2b83
Vikram S <>
2015-03-03 13:44:25 -0500

    minor tweaks

--- scc-web/src/groovy/com/freddiemac/scc/model/PoolSearch.groovy
+++ scc-web/src/groovy/com/freddiemac/scc/model/PoolSearch.groovy
@@ -13,9 +13,9 @@ static constraints = {
 	xfield  nullable: true
     poolNumber blank:true, nullable: true
     def expression = '^[A-Za-z0-9\\d]*$' 
-    cusipIdentifier validator: { val ->
+    cusipIdentifier validator: { val, obj ->
             if (val!=null && val.length() > 9) return 'PoolSearch.searchCriteria.cusipIdentifier.size'
-            if(val!=null && !val.matches(expression)) return 'PoolSearch.searchCriteria.cusipIdentifier.specialchar'            
+            if(val!=null && !val.matches(expression) && !obj.poolNumber) return 'PoolSearch.searchCriteria.cusipIdentifier.specialchar'      
 
         }
     poolNumber validator: { val, obj  ->
