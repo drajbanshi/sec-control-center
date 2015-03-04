453c3d267b89815b38216bab3d65223e935aec88
vikramsan <vikram.san@gmail.com>
2015-03-04 10:33:11 -0500

    minor tweaks

--- scc-web/src/groovy/com/freddiemac/scc/model/PoolSearch.groovy
+++ scc-web/src/groovy/com/freddiemac/scc/model/PoolSearch.groovy
@@ -14,14 +14,14 @@ static constraints = {
     poolNumber blank:true, nullable: true
     def expression = '^[A-Za-z0-9\\d]*$' 
     cusipIdentifier validator: { val, obj ->
-            if (val!=null && val.length() > 9) return 'PoolSearch.searchCriteria.cusipIdentifier.size'
-            if(val!=null && !val.matches(expression) && !obj.poolNumber) return 'PoolSearch.searchCriteria.cusipIdentifier.specialchar'      
+            if (val!=null && (val.length() > 9 || val.length() < 9) && !obj.poolNumber) return 'PoolSearch.searchCriteria.cusipIdentifier.size'
+            if(val!=null && !val.matches(expression) && !obj.poolNumber) return 'PoolSearch.searchCriteria.cusipIdentifier.specialchar'
 
         }
     poolNumber validator: { val, obj  ->
             if (!val && !obj.cusipIdentifier) return 'PoolSearch.searchCriteria.atleastonerequired'
             if (val && obj.cusipIdentifier) return 'PoolSearch.searchCriteria.bothentered'        
-            if (val!=null && val.length() > 8) return 'PoolSearch.searchCriteria.poolNumber.size'
+            if (val!=null && (val.length() > 8 || val.length() < 6))  return 'PoolSearch.searchCriteria.poolNumber.size'
             if(val!=null && !val.matches(expression)) return 'PoolSearch.searchCriteria.poolNumber.specialchar'
         }  
         
